76c4eddddc347f84b18c5bb122a52ba6a0527a9c
Arzymat <104361795+Arzymat01@users.noreply.github.com>
2023-03-03 01:18:42 +0600

    contact_us

--- newsform/contact/models.py
+++ newsform/contact/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Contact_us(models.Model):
+    full_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    subject = models.TextField()
+    phone = models.CharField(max_length=255)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.full_name
--- newsform/contact/views.py
+++ newsform/contact/views.py
@@ -1,24 +1,23 @@
 from django.core.mail import send_mail
 from django.shortcuts import render, redirect
-from .forms import ContactForm
+from .models import Contact_us
+from django.http import HttpResponse
 
 
 def contact(request):
-
     if request.method == 'POST':
-        form = ContactForm(request.POST)
+        contact = Contact_us()
+        ful_name = request.POST.get('ful_name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        contact.full_name = ful_name
+        contact.email = email
+        contact.subject = subject
+        contact.phone = phone
+        contact.message = message
+        contact.save()
+        return HttpResponse('<h1>Thanks for contact us</h1>')
 
-        if form.is_valid():
-            print('the form was valid')
-
-            send_mail('The contact form subject', 'This is the message',
-                      'arzymatarkabaev01@gmail.com', ['arzymatarkabaev01@gmail.com'])
-
-            return redirect('contact')
-        else:
-            form = ContactForm()
-
-    form = ContactForm()
-    return render(request, "contact/contact.html", {
-        'form': form
-    })
+    return render(request, 'contact/contact.html')
