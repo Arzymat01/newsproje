a31e545ffd2d4a81c04f581022c618606ba4a56e
Arzymat <104361795+Arzymat01@users.noreply.github.com>
2023-03-04 12:16:20 +0600

    delete model contact

--- newsform/authentication/admin.py
+++ newsform/authentication/admin.py
@@ -1,13 +1,6 @@
 from django.contrib import admin
+
+# Register your models here.
 from .models import Contact
 
-
-class ContactAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'email',
-                    'car_title', 'city', 'created_date')
-    list_display_link = ('first_name', 'last_name', 'email')
-    search_fields = ('first_name', 'last_name', 'email', 'car_title')
-    list_per_page = 20
-
-
-admin.site.register(Contact, ContactAdmin)
+admin.site.register(Contact)
--- newsform/authentication/models.py
+++ newsform/authentication/models.py
@@ -7,16 +7,9 @@ class Contact(models.Model):
                           primary_key=True, editable=False)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    car_id = models.CharField(max_length=500)
-    car_title = models.CharField(max_length=200)
-    customer_need = models.CharField(max_length=100)
-    city = models.CharField(max_length=100)
-    state = models.CharField(max_length=100, null=True, blank=True)
     email = models.EmailField(max_length=100)
     phone = models.CharField(max_length=100)
     message = models.TextField()
-    user_id = models.CharField(max_length=100, blank=True)
-    created_date = models.DateTimeField(blank=True, auto_now_add=True)
 
     def __str__(self) -> str:
         return self.email
