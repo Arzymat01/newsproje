5efc180b06268c8e674ab807941e98f2e61e5c51
Arzymat <104361795+Arzymat01@users.noreply.github.com>
2023-03-23 17:18:58 +0600

    apiNews

--- newsform/website/views.py
+++ newsform/website/views.py
@@ -46,36 +46,72 @@ def index(request):
 
 
 def business(request):
+    newsapi_key = "10295f4657ce4101b8697efa2fec8d97"
+
+    url = f"https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey={newsapi_key}"
+
+    c_news = requests.get(url).json()
+    a = c_news['articles']
+    desc = []
+    title = []
+    img = []
+    pbl = []
+    ctn = []
+    ur = []
+    for i in range(len(a)):
+        f = a[i]
+        title.append(f['title'])
+        desc.append(f['description'])
+        img.append(f['urlToImage'])
+        pbl.append(f['publishedAt'])
+        ctn.append(f['content'])
+        ur.append(f['url'])
+
+    mylist = zip(title, desc, img, pbl, ctn, ur)
 
     news = News.objects.all()
     context = {
+        'mylist': mylist,
         'news': news
     }
     return render(request, 'website/business.html', context)
 
 
 def health(request):
+    newsapi_key = "10295f4657ce4101b8697efa2fec8d97"
+    url = f"https://newsapi.org/v2/top-headlines?country=us&category=health&apiKey={newsapi_key}"
+
+    c_news = requests.get(url).json()
+    a = c_news['articles']
+    desc = []
+    title = []
+    img = []
+    pbl = []
+    ctn = []
+    ur = []
+    for i in range(len(a)):
+        f = a[i]
+        title.append(f['title'])
+        desc.append(f['description'])
+        img.append(f['urlToImage'])
+        pbl.append(f['publishedAt'])
+        ctn.append(f['content'])
+        ur.append(f['url'])
+
+    mylist = zip(title, desc, img, pbl, ctn, ur)
 
     news = News.objects.all()
     context = {
+        'mylist': mylist,
         'news': news
     }
     return render(request, 'website/health.html', context)
 
 
 def politika(request):
-
-    news = News.objects.all()
-    context = {
-        'news': news
-    }
-    return render(request, 'website/politika.html', context)
-
-
-def sport(request):
     newsapi_key = "10295f4657ce4101b8697efa2fec8d97"
 
-    url = f"https://newsapi.org/v2/top-headlines?country=us&apiKey={newsapi_key}"
+    url = f"https://newsapi.org/v2/top-headlines?country=us&category=general&apiKey={newsapi_key}"
 
     c_news = requests.get(url).json()
     a = c_news['articles']
@@ -100,6 +136,39 @@ def sport(request):
     context = {
         'news': news,
         'mylist': mylist,
+    }
+    return render(request, 'website/politika.html', context)
+
+
+def sport(request):
+    newsapi_key = "10295f4657ce4101b8697efa2fec8d97"
+
+    url = f"https://newsapi.org/v2/top-headlines?country=us&category=sport&apiKey={newsapi_key}"
+
+    c_news = requests.get(url).json()
+    a = c_news['articles']
+    desc = []
+    title = []
+    img = []
+    pbl = []
+    ctn = []
+    ur = []
+    for i in range(len(a)):
+        f = a[i]
+        title.append(f['title'])
+        desc.append(f['description'])
+        img.append(f['urlToImage'])
+        pbl.append(f['publishedAt'])
+        ctn.append(f['content'])
+        ur.append(f['url'])
+
+    mylist = zip(title, desc, img, pbl, ctn, ur)
+
+    news = News.objects.all()
+
+    context = {
+        'news': news,
+        'mylist': mylist,
 
 
     }
@@ -108,9 +177,30 @@ def sport(request):
 
 def technology(request):
 
+    url = f"https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=10295f4657ce4101b8697efa2fec8d97"
+
+    c_news = requests.get(url).json()
+    a = c_news['articles']
+    desc = []
+    title = []
+    img = []
+    pbl = []
+    ctn = []
+    ur = []
+    for i in range(len(a)):
+        f = a[i]
+        title.append(f['title'])
+        desc.append(f['description'])
+        img.append(f['urlToImage'])
+        pbl.append(f['publishedAt'])
+        ctn.append(f['content'])
+        ur.append(f['url'])
+
+    mylist = zip(title, desc, img, pbl, ctn, ur)
     news = News.objects.all()
     context = {
-        'news': news
+        'news': news,
+        'mylist': mylist,
     }
     return render(request, 'website/technology.html', context)
 
